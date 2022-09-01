f3d345b9fdbc6f2fce23692fca7f94ef690a3259
Piyush Lund <piyushlund349@gmail.com>
2022-09-02 01:27:04 +0530

    feat: added calculate sum method

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ const dateOfBirth = document.querySelector("#input-date-of-birth");
 const luckyNumber = document.querySelector("#input-lucky-number");
 const btnCheck = document.querySelector("#btn-check");
 
-btnCheck.addEventListener('click',function getValues(){
-    console.log(dateOfBirth.value,luckyNumber.value)
-})
+function isBirthDayLucky(){
+    const date = dateOfBirth.value;
+    const sum = calculateSum(date);
+    console.log(sum)
+}
+
+function calculateSum(date){
+    var sum=0;
+    for(var i=0;i< date.length;i++){
+        if(date[i]==="-")
+            continue
+        sum = sum + Number(date[i])
+    }
+    return sum
+}
+
+btnCheck.addEventListener('click',isBirthDayLucky)
